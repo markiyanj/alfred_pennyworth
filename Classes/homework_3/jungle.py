4605efd173e25e7c1312d990b5e7bd5e341c6557
Markiian Hashchuk <gashukm51@gmail.com>
2020-10-30 17:03:32 +0200

    add HW#7

--- Classes/homework_3/jungle.py
+++ Classes/homework_3/jungle.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from abc import abstractmethod, ABC
 from uuid import uuid4
 import random
+import csv
+import time
 
 
 class Animal(ABC):
@@ -28,7 +30,8 @@ class Predator(Animal):
             self.current_power -= 0.3 * self.max_power
         else:
             if self.speed > random_animal.speed and self.current_power > random_animal.current_power:
-                self.current_power += 40
+                self.current_power += 0.4 * self.current_power
+                jungle.remove_animal(random_animal)
                 if self.current_power > self.max_power:
                     self.current_power = self.max_power
             else:
@@ -42,7 +45,7 @@ class Herbivorous(Animal):
         if self.current_power <= 0:
             jungle.remove_animal(self)
         else:
-            self.current_power += 40
+            self.current_power += self.current_power * 0.4
             if self.current_power > self.max_power:
                 self.current_power = self.max_power
 
@@ -77,7 +80,7 @@ class Jungle:
 
 def animal_generator():
     animal_list = []
-    for i in range(10):
+    for i in range(int(input('With how many animals you want to play?: '))):
         power = random.randint(20, 100)
         speed = random.randint(20, 100)
         if random.randint(0, 1) == 0:
@@ -90,23 +93,70 @@ def animal_generator():
         yield animal
 
 
+def csv_upload(path):
+    with open(path, 'w') as csv_file:
+        field_name = ['Class', 'max_power', 'speed']
+        csv_writer = csv.DictWriter(csv_file, fieldnames=field_name, delimiter=',')
+        csv_writer.writeheader()
+        list_of_animals = animal_generator()
+        while True:
+            try:
+                animal = next(list_of_animals)
+                csv_writer.writerow({'Class': animal.__class__.__name__,
+                                     'max_power': animal.max_power,
+                                     'speed': animal.speed})
+            except StopIteration:
+                break
+
+
+def csv_load(path):
+    count = 0
+    with open(path, 'r') as read_csv:
+        csv_reader = csv.reader(read_csv)
+        for row in csv_reader:
+            if count == 0:
+                count += 1
+                continue
+            class_of_animal = row[0]
+            max_power = int(row[1])
+            speed = int(row[2])
+            if class_of_animal == "Predator":
+                animal = Predator(max_power, speed)
+                jungle.add_animal(animal)
+            else:
+                animal = Herbivorous(max_power, speed)
+                jungle.add_animal(animal)
+
+
 if __name__ == "__main__":
     # Create jungle
     # Create few animals
     # Add animals to jungle
     # Iterate throw jungle and force animals to eat until no predators left
     # animal_generator to create a random animal
-    gen_animal = animal_generator()
+
+    # gen_animal = animal_generator()
+
+    csv_upload('animal_configuration.csv')
 
     jungle = Jungle()
 
-    while True:
-        try:
-            animal = next(gen_animal)
-            jungle.add_animal(animal)
-        except StopIteration:
-            break
+    csv_load('animal_configuration.csv')
 
+    # while True:
+    #     try:
+    #         animal = next(gen_animal)
+    #         jungle.add_animal(animal)
+    #     except StopIteration:
+    #         break
+
+    print('Let`s look at the animals in the jungle ', '\n')
+    time.sleep(1)
+    for animal in jungle:
+        print(f'Class:{animal.__class__.__name__}, max_power:{animal.max_power}, speed:{animal.speed}')
+
+    time.sleep(1)
+    print('The HUNT began', '\n')
     while True:
         if any(isinstance(animal, Predator) for animal in jungle.animals.values()):
             try:
@@ -116,3 +166,11 @@ if __name__ == "__main__":
                 continue
         else:
             break
+    time.sleep(0.5)
+    print('jungle after hunting', '\n')
+    time.sleep(0.5)
+    if jungle.animals == {}:
+        print('No animals in jungle')
+    else:
+        for an in jungle.animals.values():
+            print(f'Class:{an.__class__.__name__}, current_power:{an.current_power}, speed:{an.speed}')
